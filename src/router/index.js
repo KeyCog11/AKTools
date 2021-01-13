d00dc3a5348011a9d494b1d6689eccb87d005a69
Key <keycog11@gmail.com>
2021-01-14 05:58:39 +0700

    Add vue-markdown

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import EnemyFinder from "../views/EnemyFinder.vue"
+import MarkdownViewer from "../views/MarkdownViewer.vue"
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
     path: '/enemyfinder',
     name: "EnemyFinder",
     component: EnemyFinder
+  },
+  {
+    path: '/blog/:md',
+    name: "MarkdownViewer",
+    component: MarkdownViewer
   }
 ]
 
---
+++ src/views/MarkdownViewer.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <vue-markdown>
+            {{content.default}}
+        </vue-markdown>
+    </v-container>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown'
+export default {
+    components: {
+        VueMarkdown
+    },
+    data: () => ({
+        content: null,
+    }), 
+    async mounted() {
+        let md = this.$route.params.md
+        try {
+           this.content = require("raw-loader!@/markdowns/"+md)
+        } catch {
+            this.content = "404 Not Found"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
